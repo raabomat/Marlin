c37ccf3ead5c436389a12a9c3ad6ea2ef36ad2d1
raabomat <43581692+raabomat@users.noreply.github.com>
2020-06-22 19:34:57 +0200

    Update Configuration.h
    fix z inverse movement and display charset western

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1082,7 +1082,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
@@ -1666,7 +1666,7 @@
  *
  * :['JAPANESE', 'WESTERN', 'CYRILLIC']
  */
-#define DISPLAY_CHARSET_HD44780 JAPANESE
+#define DISPLAY_CHARSET_HD44780 WESTERN
 
 /**
  * Info Screen Style (0:Classic, 1:Prusa)
